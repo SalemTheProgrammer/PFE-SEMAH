86f0a3f8cf5d3095a21b8489bee16edb63307df4
Salem Dahmani <salem.dahmani345@gmail.com>
2024-08-21 19:42:24 +0100

    Semah test

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+// src/App.js
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app-container">
+      <div className="title-container">
+        <h1>Always a Cut Above the Rest</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
